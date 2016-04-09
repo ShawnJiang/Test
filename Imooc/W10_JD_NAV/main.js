40a75bf55325f3725ac5ecf2e34384f89adeb98e
ShawnJiang <jsj609079020@gmail.com>
2016-04-10 05:35:18 +0800

    1

---
+++ Imooc/W10_JD_NAV/main.js
@@ -0,0 +1,15 @@
+// window.onload=function(){
+// 	var nav_con = document.getElementById("nav_con"),
+// 		lis = nav_con.children,
+// 		nav_list_box = document.getElementById("nav_list_box"),
+// 		divs = nav_list_box.children;
+// 	for (var i = 0; i < lis.length; i++) {
+// 		lis[i].index = i;
+// 		lis[i].onmouseover=function(){
+// 			divs[this.index].style.display="block";
+// 		}
+// 		lis[i].onmouseout=function(){
+// 			// divs[this.index].style.display="none";
+// 		}
+// 	}
+// }
